50bf2e298b74cf75649d86b2cc8c73e52c482a61
jeantardelli <jeantardelli@gmail.com>
2021-12-07 22:59:22 +0100

    Javascript - Pt. II

---
+++ Javascript/loops.js
@@ -0,0 +1,8 @@
+/*Each if these code segments calculates the sum of 0 to 9*/
+var i=0, sum=0;
+while (i<10) {sum+=i; i++;}
+
+var i=0, sum=0
+do {sum+=i++;} while(i<10);
+
+for (var i=0, sum=0, i<10; i++) sum+=i;
